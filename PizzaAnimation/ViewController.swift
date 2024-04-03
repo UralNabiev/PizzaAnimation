242772edd7cf5295c5175c75291a588a608ead69
UralNabiev <devuralnabiev@gmail.com>
2024-04-03 08:06:59 +0500

    Initial Commit

---
+++ PizzaAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaAnimation
+//
+//  Created by O'ral Nabiyev on 03/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
